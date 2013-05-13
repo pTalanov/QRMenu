86c15c934c97afc57777cd468ff90f6ea02f7a53
Pavel Talanov <talanov.pavel@gmail.com>
2013-05-13 21:35:25 +0400

    Remove gen/* from vcs

--- gen/ru/spbau/qrmenu/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package ru.spbau.qrmenu;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- gen/ru/spbau/qrmenu/R.java
+++
@@ -1,5 +0,0 @@
-package ru.spbau.qrmenu;
-
-/* This stub is for using by IDE only. It is NOT the R class actually packed into APK */
-public final class R {
-}
