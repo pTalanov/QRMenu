b977b2bc43aacb0ec3409424929db0ffe9253730
Sergey A. Savenko <sergey.a.savenko@gmail.com>
2013-05-13 15:43:55 +0400

    added test QR read activity call and print of what's read

--- src/ru/spbau/qrmenu/QRMenuMainActivity.java
+++ src/ru/spbau/qrmenu/QRMenuMainActivity.java
@@ -5,8 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class QRMenuMainActivity extends Activity {
+
+    private static final int SCAN_QR_CODE_REQUEST_CODE = 0;
+
     /**
      * Called when the activity is first created.
      */
@@ -19,9 +23,27 @@ public class QRMenuMainActivity extends Activity {
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), com.google.zxing.client.android.CaptureActivity.class);
-                startActivity(intent);
+                Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+                intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+                startActivityForResult(intent, SCAN_QR_CODE_REQUEST_CODE);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (SCAN_QR_CODE_REQUEST_CODE == requestCode) {
+            TextView tv = (TextView) findViewById(R.id.textView);
+            switch (resultCode) {
+                case RESULT_OK : {
+                    tv.setText(data.getStringExtra("SCAN_RESULT"));
+                    break;
+                }
+                default : {
+                    tv.setText("Canceled");
+                    break;
+                }
+            }
+        }
     }
 }
