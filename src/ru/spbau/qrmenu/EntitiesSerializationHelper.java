b7d913ee2f7911356b4b332bc692194331b22888
Sergey A. Savenko <sergey.a.savenko@gmail.com>
2013-05-13 20:08:38 +0400

    added EntitiesSerializtationHelper;

---
+++ src/ru/spbau/qrmenu/EntitiesSerializationHelper.java
@@ -0,0 +1,30 @@
+package ru.spbau.qrmenu;
+
+import ru.spbau.qrmenu.entities.RestaurantMenuItem;
+import ru.spbau.qrmenu.entities.RestaurantTable;
+
+/**
+ * @author Sergey A. Savenko
+ */
+public final class EntitiesSerializationHelper {
+
+    private EntitiesSerializationHelper() {
+    }
+
+    public static RestaurantMenuItem parseMenuItem(String json) {
+        return null;
+    }
+
+    public static String serializeMenuItem(RestaurantMenuItem rmi) {
+        return null;
+    }
+
+    public static RestaurantTable parseTable(String json) {
+        return null;
+    }
+
+    public static String serializeTable(RestaurantTable table) {
+        return null;
+    }
+
+}
