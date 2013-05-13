29a83a26b5e9dddce6d1398ff8429966e4ce3009
Sergey A. Savenko <sergey.a.savenko@gmail.com>
2013-05-13 13:34:57 +0400

    added helloworld project

---
+++ qrmenu/gen/ru/spbau/qrmenu/Manifest.java
@@ -0,0 +1,5 @@
+package ru.spbau.qrmenu;
+
+/* This stub is for using by IDE only. It is NOT the Manifest class actually packed into APK */
+public final class Manifest {
+}
---
+++ qrmenu/gen/ru/spbau/qrmenu/R.java
@@ -0,0 +1,5 @@
+package ru.spbau.qrmenu;
+
+/* This stub is for using by IDE only. It is NOT the R class actually packed into APK */
+public final class R {
+}
