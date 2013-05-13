b059572446d74820c397a997179ce8c8e4b547f7
Sergey A. Savenko <sergey.a.savenko@gmail.com>
2013-05-13 15:14:16 +0400

    added sample xzing code

--- src/ru/spbau/qrmenu/QRMenuMainActivity.java
+++ src/ru/spbau/qrmenu/QRMenuMainActivity.java
@@ -1,7 +1,10 @@
 package ru.spbau.qrmenu;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class QRMenuMainActivity extends Activity {
     /**
@@ -11,5 +14,14 @@ public class QRMenuMainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Button b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), com.google.zxing.client.android.CaptureActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
